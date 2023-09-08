863d57047ed26fa3b04e952de946e650b4bc22fb
AndyXi163 <144465361+AndyXi163@users.noreply.github.com>
2023-09-08 15:18:53 -0400

    Create utils_tests.py

---
+++ utils_tests.py
@@ -0,0 +1,11 @@
+from utils import *
+
+utils newUtil;
+
+print(newUtil.reversed(newUtil, 'abcde'));
+print(newUtil.reversed(newUtil, 3.14159));
+print(newUtil.reversed(newUtil, 123));
+
+print(newUtil.formatter(newUtil, 'abcde'));
+print(newUtil.formatter(newUtil, 3.14159));
+print(newUtil.formatter(newUtil, 123));
